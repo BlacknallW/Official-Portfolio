f2222c04e44c38b2dad057509a25cf44a3dc1802
William Blacknall <williamblacknall@gmail.com>
2019-10-30 13:58:13 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+const es6Renderer = require('express-es6-template-engine')
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+
+module.exports = app;
